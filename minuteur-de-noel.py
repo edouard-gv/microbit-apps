e561ec5672fefe0019ddf136bda964ee7c6bde51
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2023-01-03 20:49:48 +0100

    first demo feedbacks

--- minuteur-de-noel.py
+++ minuteur-de-noel.py
@@ -6,21 +6,34 @@ global timer
 global beepFrame
 global timerLaunched
 global lum
+global frame
+is_sound_on = False
 
 TIMER_INC = 60
 TIMER_DELAY = 1000
+FRAME_FREQ = 5
+
+def switch_sound():
+    global is_sound_on
+    is_sound_on = not is_sound_on
+    if is_sound_on:
+        display.show(Image.MUSIC_QUAVER, wait=False)
+    else:
+        display.show(Image.NO, wait=False)
 
 def raz(razTimer = True):
     global timer
     global beepFrame
     global timerLaunched
     global lum
+    global frame
     
     if razTimer:
         timer = 0
     beepFrame = 5
     lum = 0
-    timerLaunched = False  
+    timerLaunched = False
+    frame = 0
     display.clear()
 
 def print_time(timer):
@@ -42,38 +55,51 @@ def print_time(timer):
         display.set_pixel(i, 4, 7)
         
 
-
 raz()
 
 while True:
-    if button_b.was_pressed():
+
+    if pin_logo.is_touched():
         raz()
     
-    clicks = button_a.get_presses()
-    if clicks > 0:
-        if timer < 30:
-            clicks = clicks - 0.5
+    if button_b.was_pressed():
+        switch_sound()
+
+    if button_a.was_pressed():
         raz(False)
-        timer = (TIMER_INC*clicks+timer) % (4*TIMER_INC);
+        if timer == 0:
+            timer = TIMER_INC/2 + 5
+        elif timer < 36:
+            timer = timer + TIMER_INC/2
+        else:
+            timer = (timer + TIMER_INC) % (4*TIMER_INC);
         timerLaunched = True;
 
     if timerLaunched:
-        if timer > 0:
-            display.clear()
-            print_time(timer)
-            timer = timer - 1
-            sleep(TIMER_DELAY)
+            if timer > 0:
+                if frame == 0:
+                    display.clear()
+                    print_time(timer)
+                    timer = timer - 1
+                
+                frame = (frame + 1) % FRAME_FREQ
+                sleep(1000/FRAME_FREQ)
+        
+            else:
+                display.show(Image.HEART)
+                if is_sound_on:
+                    audio.play(Sound.GIGGLE)
+                sleep(1000)
+                raz()
+                power.deep_sleep(wake_on=button_a)
 
-        else:
-            display.show(Image.HEART)
-            audio.play(Sound.GIGGLE)
-            sleep(1000)
-            raz()
-            power.off()
+            
     else:
         display.set_pixel(2, 2, abs(lum-9))
         sleep(70)
         lum = (lum + 1) % 18
+
+
         
             
         
